8768d2f722ba26e5e665831b48751282adc86165
sachintha2882 <sachinthadhitinayaka@gmail.com>
2026-06-27 07:52:28 +0530

    Add BCrypt for Password

--- src/main/java/com/web/liveshades/Service/UserService.java
+++ src/main/java/com/web/liveshades/Service/UserService.java
@@ -3,6 +3,7 @@ package com.web.liveshades.Service;
 import com.web.liveshades.Model.User;
 import com.web.liveshades.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,26 +14,43 @@ public class UserService {
     @Autowired
     private UserRepo userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Create User
     public User saveUser(User user) {
+
+        // Encrypt password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 
+    // Get All Users
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    // Get User By ID
     public User getUserById(Integer id) {
         return userRepository.findById(id).orElse(null);
     }
 
+    // Update User
     public User updateUser(Integer id, User user) {
 
         User existingUser = userRepository.findById(id).orElse(null);
 
         if (existingUser != null) {
+
             existingUser.setUsername(user.getUsername());
             existingUser.setEmail(user.getEmail());
-            existingUser.setPassword(user.getPassword());
+
+            // Encrypt only if password is changed
+            if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+                existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
+            }
+
             existingUser.setRole(user.getRole());
             existingUser.setEnabled(user.isEnabled());
 
@@ -42,6 +60,7 @@ public class UserService {
         return null;
     }
 
+    // Delete User
     public void deleteUser(Integer id) {
         userRepository.deleteById(id);
     }
